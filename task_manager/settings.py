d2d6062f4f43ddf54aab889e827081ec4e449a65
Наталья Тайгачева <dupist@Air-Natala.bbrouter>
2024-05-06 17:07:56 +0700

    Add DATABASE_URL in settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -77,6 +77,8 @@ WSGI_APPLICATION = "task_manager.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+DATABASE_URL = os.getenv('DATABASE_URL')
+
 
 DATABASES = {
     'default': dj_database_url.parse(DATABASE_URL)
