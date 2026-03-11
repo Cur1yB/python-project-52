c661042399a61eb389ad18f7d1f281bce1174ebd
Alexandr <alexandr.ignatev1996@gmail.com>
2026-03-11 18:51:00 +0700

    Add new hosts in ALLOWED_HOSTS

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -34,7 +34,12 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'webserver',
+    'python-project-52-w5q1.onrender.com'
+]
 
 AUTH_USER_MODEL = "users.User"
 
